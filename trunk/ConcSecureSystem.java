d51f0e2608653accb397ef6d157bdeacc02c9a30
ccunni <ccunni@1982017e-01d6-11de-bc25-5f29055a2a2d>
2009-02-27 03:17:18 +0000

    done.

--- trunk/ConcSecureSystem.java
+++ trunk/ConcSecureSystem.java
@@ -1,80 +1,211 @@
 import java.util.*;
-
+import java.io.*;
 
 public class ConcSecureSystem 
 {
 	public static void main(String[] args)
 	{
-//	   CREATE_SUBJECT subject_name subject_role
-//	   CREATE_OBJECT subject_name object_name object_type
-//	   READ subject_name object_name 
-//	   WRITE subject_name object_name value 
-//	   DOWNGRADE subject_name object_name object_type
+		ArrayList<Instruction> instructions = readInput();
+		Firewall fw = new Firewall();
+		if(instructions.get(0).command != Instruction.Commands.AUDIT)
+		{
+			System.out.println("Must give audit as first command... exiting.");
+			System.exit(0);
+		}
 		
+		for(Instruction i: instructions)
+			fw.attempt(i);
+		
+		fw.printAudit();
+	}
+	
+	public static ArrayList<Instruction> readInput()
+	{
+		//populating hashes containing static roles and types
+		Role.populateHash();
+		Type.populateHash();
+		
+		ArrayList<Instruction> result = new ArrayList<Instruction>();
+		Scanner input = null;
+		try 
+		{
+			input = new Scanner(new File("Instructions"));
+		}
+		catch (FileNotFoundException e1) 
+		{
+			e1.printStackTrace();
+			System.exit(0);
+		}
+		
+		while(input.hasNext())
+		{
+			String line = input.nextLine().toLowerCase();
+			if (line.equals(""))
+				continue;
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			
+			ArrayList<String> tokens = new ArrayList<String>();
+			while(tokenizer.hasMoreTokens())
+				tokens.add(tokenizer.nextToken());
+			
+			Instruction.Commands command = Instruction.Commands.BAD_INSTRUCTION;
+			ArrayList<Object> instrArgs = new ArrayList<Object>();
+		
+			try
+			{
+				if(tokens.get(0).equals("audit") && tokens.size() == 2)
+				{
+					command = command.AUDIT;
+					instrArgs.add(tokens.get(1));
+				}
+				else if(tokens.get(0).equals("create_subject") && tokens.size() == 3)
+				{
+					command = command.CREATE_SUBJECT;
+					
+					instrArgs.add(tokens.get(1)); 
+					Role role = Role.roles.get(tokens.get(2).toUpperCase());
+					if(role == null) //invalid role
+						throw new Exception();
+					instrArgs.add(role);
+				}
+				else if(tokens.get(0).equals("create_object") && tokens.size() == 4)
+				{
+					command = command.CREATE_OBJECT;
+					instrArgs.add(tokens.get(1));
+					instrArgs.add(tokens.get(2));
+					Type type = Type.types.get(tokens.get(3).toUpperCase());
+					if(type == null) //invalid type
+						throw new Exception();
+					instrArgs.add(type);
+				}
+				else if(tokens.get(0).equals("read") && tokens.size() == 3)
+				{
+					command = command.READ;
+					instrArgs.add(tokens.get(1));
+					instrArgs.add(tokens.get(2));
+				}
+				else if(tokens.get(0).equals("write") && tokens.size() == 4)
+				{
+					command = command.WRITE;
+					instrArgs.add(tokens.get(1));
+					instrArgs.add(tokens.get(2));
+					instrArgs.add(tokens.get(3));
+				}
+				else if(tokens.get(0).equals("downgrade") && tokens.size() == 4)
+				{
+					command = command.DOWNGRADE;
+					instrArgs.add(tokens.get(1));
+					instrArgs.add(tokens.get(2));
+					Type type = Type.types.get(tokens.get(3).toUpperCase());
+					if(type == null) //invalid type
+						throw new Exception();
+					instrArgs.add(type);
+				}
+				else //bad command
+				{
+					throw new Exception();
+				}
+			}
+			catch(Exception e)
+			{
+				command = command.BAD_INSTRUCTION;
+			}
+			result.add(new Instruction(command, instrArgs));
+		}
+		return result;
+	}
+}
+
+
+class UnitTests
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Testing... ");
+		testPerms();
+		testDowngrade();
+		
+		System.out.println("done.");
+	}
+	
+	public static void testDowngrade()
+	{
 		Firewall wall = new Firewall();
 		
 		ArrayList instrArgs = new ArrayList();
 		instrArgs.add("SystemLog");
 		Instruction aud = new Instruction(Instruction.Commands.AUDIT, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 		instrArgs.clear();
 		
 		
 		instrArgs = new ArrayList(Arrays.asList(new Object[]{"sysCntrler", Role.SYS_CONTRLR}));
 		aud = new Instruction(Instruction.Commands.CREATE_SUBJECT, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 		
 		instrArgs = new ArrayList(Arrays.asList(new Object[]{"appDever", Role.APP_DEV}));
 		aud = new Instruction(Instruction.Commands.CREATE_SUBJECT, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 		
 		instrArgs = new ArrayList(Arrays.asList(new Object[]{"appDever", "devCode", Type.DEV_CODE}));
 		aud = new Instruction(Instruction.Commands.CREATE_OBJECT, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 		
 		
 		instrArgs = new ArrayList(Arrays.asList(new Object[]{"sysCntrler", "devCode", Type.PROD_DATA}));
 		aud = new Instruction(Instruction.Commands.DOWNGRADE, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 
 		instrArgs = new ArrayList(Arrays.asList(new Object[]{"sysCntrler", "devCode", Type.PROD_CODE}));
 		aud = new Instruction(Instruction.Commands.DOWNGRADE, instrArgs);
-		wall.executeInstruction(aud);
+		wall.attempt(aud);
 		
+		wall.attempt(aud);
+		String expected = "AUDIT SystemLog SYS_LOG\nSuccessful.\n\nCREATE_SUBJECT sysCntrler SYS_CONTRLR\nSuccessful.\n\nCREATE_SUBJECT appDever APP_DEV\nSuccessful.\n\nCREATE_OBJECT appDever devCode DEV_CODE\nSuccessful.\n\nDOWNGRADE sysCntrler devCode PROD_DATA\nFailure. Cannot downgrade from DEV_CODE to PROD_DATA\n\nDOWNGRADE sysCntrler devCode PROD_CODE\nSuccessful.\n\nDOWNGRADE sysCntrler devCode PROD_CODE\nFailure. Cannot downgrade from PROD_CODE to PROD_CODE\n\n";
+		String result = wall.getAudit();
+		assert (expected.equals(result));
+	}
+	
+	public static void testPerms()
+	{
+		assert(Role.ORDINARY.cantBoth(Type.DEV_CODE));
+		assert(Role.ORDINARY.canOnlyRead(Type.PROD_CODE));
+		assert(Role.ORDINARY.canBoth(Type.PROD_DATA));
+		assert(Role.ORDINARY.cantBoth(Type.TOOLS));
+		assert(Role.ORDINARY.canOnlyRead(Type.SYS_CODE));
+		assert(Role.ORDINARY.cantBoth(Type.SYS_CODE_MOD));
+		assert(Role.ORDINARY.canOnlyWrite(Type.SYS_LOG));
 		
-		/*
-		instrArgs.remove(0);
-		instrArgs.add(0, "ImaUser");
+		assert(Role.APP_DEV.canBoth(Type.DEV_CODE));
+		assert(Role.APP_DEV.cantBoth(Type.PROD_CODE));
+		assert(Role.APP_DEV.cantBoth(Type.PROD_DATA));
+		assert(Role.APP_DEV.canOnlyRead(Type.TOOLS));
+		assert(Role.APP_DEV.canOnlyRead(Type.SYS_CODE));
+		assert(Role.APP_DEV.cantBoth(Type.SYS_CODE_MOD));
+		assert(Role.APP_DEV.canOnlyWrite(Type.SYS_LOG));
 		
-		instrArgs.add(Role.ORDINARY);
-		aud = new Instruction(Instruction.Commands.CREATE_SUBJECT, instrArgs);
-		wall.executeInstruction(aud);
-		instrArgs.clear();
+		assert(Role.SYS_PROGMR.cantBoth(Type.DEV_CODE));
+		assert(Role.SYS_PROGMR.cantBoth(Type.PROD_CODE));
+		assert(Role.SYS_PROGMR.cantBoth(Type.PROD_DATA));
+		assert(Role.SYS_PROGMR.canOnlyRead(Type.TOOLS));
+		assert(Role.SYS_PROGMR.canOnlyRead(Type.SYS_CODE));
+		assert(Role.SYS_PROGMR.canBoth(Type.SYS_CODE_MOD));
+		assert(Role.SYS_PROGMR.canOnlyWrite(Type.SYS_LOG));
 		
-
-		instrArgs.add("ImaUser");
-		instrArgs.add("fooObj");
-		instrArgs.add(Type.PROD_DATA);
-		aud = new Instruction(Instruction.Commands.CREATE_OBJECT, instrArgs);
-		wall.executeInstruction(aud);
+		assert(Role.SYS_AUDTR.cantBoth(Type.DEV_CODE));
+		assert(Role.SYS_AUDTR.cantBoth(Type.PROD_CODE));
+		assert(Role.SYS_AUDTR.cantBoth(Type.PROD_DATA));
+		assert(Role.SYS_AUDTR.cantBoth(Type.TOOLS));
+		assert(Role.SYS_AUDTR.canOnlyRead(Type.SYS_CODE));
+		assert(Role.SYS_AUDTR.cantBoth(Type.SYS_CODE_MOD));
+		assert(Role.SYS_AUDTR.canOnlyWrite(Type.SYS_LOG));
 		
-		instrArgs.remove(1);
-		instrArgs.add(1, "SystemLog");
-		aud = new Instruction(Instruction.Commands.CREATE_OBJECT, instrArgs);
-		wall.executeInstruction(aud);
-		
-		instrArgs.remove(2);
-//		aud = new Instruction(Instruction.Commands.READ, instrArgs);
-//		wall.executeInstruction(aud);
-
-		instrArgs.add("value stuff");
-		aud = new Instruction(Instruction.Commands.WRITE, instrArgs);
-		wall.executeInstruction(aud);
-		*/
-		
-		
-		
-		wall.printAudit();
+		assert(Role.SYS_CONTRLR.cantBoth(Type.DEV_CODE));
+		assert(Role.SYS_CONTRLR.cantBoth(Type.PROD_CODE));
+		assert(Role.SYS_CONTRLR.cantBoth(Type.PROD_DATA));
+		assert(Role.SYS_CONTRLR.cantBoth(Type.TOOLS));
+		assert(Role.SYS_CONTRLR.canOnlyRead(Type.SYS_CODE));
+		assert(Role.SYS_CONTRLR.cantBoth(Type.SYS_CODE_MOD));
+		assert(Role.SYS_CONTRLR.canOnlyWrite(Type.SYS_LOG));
 	}
 }
-
--- trunk/Firewall.java
+++ trunk/Firewall.java
@@ -19,129 +19,127 @@ public class Firewall
 
 	public void printAudit()
 	{
-		//TODO are failures verbose enough?
 		System.out.println(audit.getData());
 	}
 	
-	private void runAudit(String object_name)
+	public String getAudit()
 	{
-		
+		return audit.getData();
 	}
 	
-	public void executeInstruction(Instruction instr)
+	public void attempt(Instruction instr)
 	{
 		Instruction.Commands command = instr.command;
-		String auditString = instr.toString() + "\n";
+		String auditString = instr + "\n"; //describes current instruction
 		
 		if(command.equals(Instruction.Commands.AUDIT)) 
 		{
-			myCore.coreObjects.put(instr.object_name, audit);
-			objLvls.put(instr.object_name, instr.object_type);
-			auditString += "Successful.";
+			if(!myCore.coreObjects.containsValue(audit))
+			{
+				myCore.execute(instr);
+				auditString += "Successful.";
+			}
+			else
+				auditString += "Failure. Audit object already exists.";
 		}
+		
 		else if(command.equals(Instruction.Commands.CREATE_SUBJECT))
 		{
 			if(!subLvls.containsKey(instr.subject_name)) //subject doesn't already exist
 			{
-				subLvls.put(instr.subject_name, instr.subject_role);
+				myCore.execute(instr);
 				auditString += "Successful.";
 			}
-			else
-				auditString += "Failure. Subject " + instr.subject_name + " already exists.";
+			else auditString += "Failure. Subject " + instr.subject_name + " already exists.";
 		}
+		
 		else if(command.equals(Instruction.Commands.CREATE_OBJECT))
 		{
 			if(!objLvls.containsKey(instr.object_name)) //object doesn't already exist
 			{
-				if(subLvls.get(instr.subject_name).canWrite(instr.object_type))
+				if(subLvls.get(instr.subject_name).canWrite(instr.object_type)) //has permission
 				{
-				
-					objLvls.put(instr.object_name, instr.object_type);
-					myCore.coreObjects.put(instr.object_name, new SecureObject());
+					myCore.execute(instr);
 					auditString += "Successful.";
 				}
-				else
-					auditString += "Failure. Subject " + instr.subject_name + " does not have write access.";
+				else auditString += "Failure. Subject " + instr.subject_name + " does not have write access.";
 			}
-			else
-				auditString += "Failure. Object " + instr.object_name + " already exists.";
+			else auditString += "Failure. Object " + instr.object_name + " already exists.";
 		}
+		
 		else if(command.equals(Instruction.Commands.READ))
 		{
-			if(subLvls.containsKey(instr.subject_name))
+			if(subLvls.containsKey(instr.subject_name)) //subject exists
 			{
 				if(objLvls.containsKey(instr.object_name)) //object exists 	
 				{
-					if(subLvls.get(instr.subject_name).canRead(objLvls.get(instr.object_name)))
+					if(subLvls.get(instr.subject_name).canRead(objLvls.get(instr.object_name))) //permission
 					{
+						myCore.execute(instr);
 						auditString += "Successful.";
 					}
-					else
-						auditString += "Failure. Subject " + instr.subject_name + " does not have read access.";
+					else auditString += "Failure. Subject " + instr.subject_name + " does not have read access.";
 				}
-				else
-					auditString += "Failure. Object " + instr.object_name + " does not exist.";
+				else auditString += "Failure. Object " + instr.object_name + " does not exist.";
 			}
-			else 
-				auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
-		}	
+			else auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
+		}
+		
 		else if(command.equals(Instruction.Commands.WRITE))
 		{
-			if(subLvls.containsKey(instr.subject_name))
+			if(subLvls.containsKey(instr.subject_name)) //subject exists
 			{
 				if(objLvls.containsKey(instr.object_name)) //object exists 	
 				{
-					if(subLvls.get(instr.subject_name).canWrite(objLvls.get(instr.object_name)))
+					if(subLvls.get(instr.subject_name).canWrite(objLvls.get(instr.object_name))) //has permission
 					{
+						myCore.execute(instr);
 						auditString += "Successful.";
 					}
-					else
-						auditString += "Failure. Subject " + instr.subject_name + " does not have write access.";
+					else auditString += "Failure. Subject " + instr.subject_name + " does not have write access.";
 				}
-				else
-					auditString += "Failure. Object " + instr.object_name + " does not exist.";
+				else auditString += "Failure. Object " + instr.object_name + " does not exist.";
 			}
-			else 
-				auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
-		
+			else auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
 		}
+		
 		else if(command.equals(Instruction.Commands.DOWNGRADE))
 		{
-			if(subLvls.containsKey(instr.subject_name))
+			if(subLvls.containsKey(instr.subject_name)) //subject exists
 			{
 				if(objLvls.containsKey(instr.object_name)) //object exists 	
 				{
-					if(subLvls.get(instr.subject_name) == Role.SYS_CONTRLR)
+					if(subLvls.get(instr.subject_name) == Role.SYS_CONTRLR) //proper role
 					{
 						Type currentLvl = objLvls.get(instr.object_name);
 						Type newLvl = instr.object_type;
 						
-						if(
-						(currentLvl == Type.DEV_CODE && newLvl == Type.PROD_CODE)
+						if(	//2 valid downgrades
+						(currentLvl == Type.DEV_CODE && newLvl == Type.PROD_CODE) 
 						||
 						(currentLvl == Type.SYS_CODE_MOD && newLvl == Type.SYS_CODE))
 						{	
-							objLvls.put(instr.object_name, newLvl);
+							myCore.execute(instr);
 							auditString += "Successful.";
 						}
-						else
-							auditString += "Failure. Cannot downgrade from " + currentLvl + " to " + newLvl;
+						else auditString += "Failure. Cannot downgrade from " + currentLvl + " to " + newLvl;
 					}
-					else
-						auditString += "Failure. Subject " + instr.subject_name + " is not a System Controller.";
+					else auditString += "Failure. Subject " + instr.subject_name + " is not a System Controller.";
 				}
-				else
-					auditString += "Failure. Object " + instr.object_name + " does not exist.";
+				else auditString += "Failure. Object " + instr.object_name + " does not exist.";
 			}
-			else 
-				auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
+			else auditString += "Failure. Subject " + instr.subject_name + " does not exist.";
 		}
+		else //bad instr
+			auditString = "Failure, BAD INSTRUCTION.";
 		
-		audit.addData(auditString);
+		//update audit
+		audit.appendToData(auditString);
 	}
 
 	private class Core
 	{
+		//where objects reside
 		private HashMap<String, SecureObject> coreObjects;
 		
 		public Core() 
@@ -149,7 +147,12 @@ public class Firewall
 			coreObjects = new HashMap<String, SecureObject>();
 		}
 		
-		public void executeInstruction(Instruction instr)
+		/**
+		 * Blindly executes command passed in, relying
+		 * on Firewall to have checked the permissions.
+		 * @param instr instruction to execute
+		 */
+		public void execute(Instruction instr)
 		{
 			Instruction.Commands command = instr.command;
 			
@@ -192,9 +195,10 @@ public class Firewall
 			data = "";
 		}
 		
-		public void addData(String newData)
+		//special method useful for updating audit log
+		public void appendToData(String newData)
 		{
-			data += (newData + "\n\n");
+			data += (newData + "\n\n"); //extra newlines for spacing
 		}
 		
 		public String getData()
